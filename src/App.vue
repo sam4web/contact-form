93aecbefcd6f849bbae1bb727072709225b8b483
Sijal Manandhar <sijal.m06@gmail.com>
2024-06-30 21:11:28 +0545

    added `SuccessModal` component, separate scss file for components

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <script setup>
+import { ref } from 'vue'
+
 import ContactForm from '@/components/ContactForm.vue'
+import SuccessModal from '@/components/SuccessModal.vue'
+
+const showModal = ref(false)
 </script>
 
 <template>
-  <main className="size-full min-h-screen flex-center">
+  <main className="size-full min-h-screen flex-center relative">
+    <SuccessModal :showModal="showModal" />
     <div class="w-full section-spacing">
       <div class="max-w-[820px] mx-auto w-full">
         <ContactForm />
---
+++ src/components/SuccessModal.vue
@@ -0,0 +1,15 @@
+<script setup>
+const props = defineProps(['showModal'])
+</script>
+
+<template>
+  <section class="absolute top-[3.25%] px-2 z-[2]" :class="{ hidden: !props.showModal }">
+    <div class="modal-container space-y-2.5">
+      <div class="flex items-center space-x-3">
+        <img src="@/assets/icons/success-check.svg" alt="success-icon" />
+        <h3>message sent!</h3>
+      </div>
+      <p>Thanks for completing the form. We'll be in touch soon!</p>
+    </div>
+  </section>
+</template>
