bb3d5347aa397f37da62fa059fe84639a2882409
Sijal Manandhar <sijal.m06@gmail.com>
2024-06-30 14:34:04 +0545

    error state & radio input

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -12,29 +12,55 @@ function handleSubmit() {
       <div class="flex-section">
         <div class="input-container">
           <label for="first-name">first name</label>
-          <input type="text" name="first-name" id="first-name" value="John" />
+          <input class="input-error" type="text" name="first-name" id="first-name" value="John" />
+          <p class="error-text">This field is required</p>
         </div>
         <div class="input-container">
           <label for="last-name">last name</label>
-          <input type="text" name="last-name" id="last-name" value="Appleseed" />
+          <input
+            class="input-error"
+            type="text"
+            name="last-name"
+            id="last-name"
+            value="Appleseed"
+          />
+          <p class="error-text">This field is required</p>
         </div>
       </div>
 
       <div class="input-container">
         <label for="email">email address</label>
-        <input type="email" name="email" id="email" value="email@example.com" />
+        <input class="input-error" type="email" name="email" id="email" value="email@example.com" />
+        <p class="error-text">Please enter a valid email address</p>
       </div>
 
-      <!-- <div class="flex-section"></div> -->
+      <div class="input-container">
+        <label>query type</label>
+        <div class="flex-section">
+          <div class="input-radio">
+            <input type="radio" name="query-type" id="general-enquiry" />
+            <label for="general-enquiry">General Enquiry</label>
+          </div>
+          <div class="input-radio">
+            <input type="radio" name="query-type" id="support-request" />
+            <label for="support-request">Support Request</label>
+          </div>
+        </div>
+        <p class="error-text">Please select a query type</p>
+      </div>
 
       <div class="input-container">
         <label for="message">message</label>
-        <textarea name="message" id="message"></textarea>
+        <textarea class="input-error" name="message" id="message"></textarea>
+        <p class="error-text">This field is required</p>
       </div>
 
-      <div class="input-container flex items-center">
-        <input type="checkbox" name="agreed" id="agreed" />
-        <label for="agreed">I consent to being contacted by the team</label>
+      <div class="input-container">
+        <div class="flex items-center space-x-4">
+          <input type="checkbox" name="agreed" id="agreed" />
+          <label for="agreed">I consent to being contacted by the team</label>
+        </div>
+        <p class="error-text">To submit this form, please consent to being contacted</p>
       </div>
 
       <button class="btn" type="submit">Submit</button>
