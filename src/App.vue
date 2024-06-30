5de8495f96816497c9deb28ffb4ae4e6a3017d54
Sijal Manandhar <sijal.m06@gmail.com>
2024-06-30 18:08:57 +0545

    fixed radio selection, styling & layout

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,13 @@ import ContactForm from '@/components/ContactForm.vue'
 </script>
 
 <template>
-  <main class="w-full h-screen flex-center">
-    <ContactForm />
+  <!-- <main class="w-full h-screen flex-center section-spacing"></main> -->
+
+  <main className="size-full min-h-screen flex-center">
+    <div class="w-full section-spacing">
+      <div class="max-w-[820px] mx-auto w-full">
+        <ContactForm />
+      </div>
+    </div>
   </main>
 </template>
--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -1,6 +1,21 @@
 <script setup>
+import { reactive } from 'vue'
+
+const formData = reactive({
+  firstName: '',
+  lastName: '',
+  email: '',
+  queryType: '',
+  message: '',
+  checked: false
+})
+
+function checkSelected(value) {
+  return value === formData.queryType
+}
+
 function handleSubmit() {
-  console.log('Submitted !')
+  console.log(JSON.stringify(formData))
 }
 </script>
 
@@ -12,55 +27,70 @@ function handleSubmit() {
       <div class="flex-section">
         <div class="input-container">
           <label for="first-name">first name</label>
-          <input class="input-error" type="text" name="first-name" id="first-name" value="John" />
-          <p class="error-text">This field is required</p>
+          <input type="text" name="first-name" id="first-name" v-model="formData.firstName" />
+          <!-- <p class="error-text">This field is required</p> -->
         </div>
         <div class="input-container">
           <label for="last-name">last name</label>
-          <input
-            class="input-error"
-            type="text"
-            name="last-name"
-            id="last-name"
-            value="Appleseed"
-          />
-          <p class="error-text">This field is required</p>
+          <input type="text" name="last-name" id="last-name" v-model="formData.lastName" />
+          <!-- <p class="error-text">This field is required</p> -->
         </div>
       </div>
 
       <div class="input-container">
         <label for="email">email address</label>
-        <input class="input-error" type="email" name="email" id="email" value="email@example.com" />
-        <p class="error-text">Please enter a valid email address</p>
+        <input type="email" name="email" id="email" v-model="formData.email" />
+        <!-- <p class="error-text">Please enter a valid email address</p> -->
       </div>
 
       <div class="input-container">
         <label>query type</label>
         <div class="flex-section">
-          <div class="input-radio">
-            <input type="radio" name="query-type" id="general-enquiry" />
-            <label for="general-enquiry">General Enquiry</label>
-          </div>
-          <div class="input-radio">
-            <input type="radio" name="query-type" id="support-request" />
-            <label for="support-request">Support Request</label>
-          </div>
+          <label
+            :class="{ 'input-radio-selected': checkSelected('general-enquiry') }"
+            class="input-radio"
+            for="general-enquiry"
+          >
+            <input
+              type="radio"
+              name="query-type"
+              id="general-enquiry"
+              value="general-enquiry"
+              v-model="formData.queryType"
+            />
+            <p>General Enquiry</p>
+          </label>
+
+          <label
+            :class="{ 'input-radio-selected': checkSelected('support-request') }"
+            class="input-radio"
+            for="support-request"
+          >
+            <input
+              type="radio"
+              name="query-type"
+              id="support-request"
+              value="support-request"
+              v-model="formData.queryType"
+            />
+            <p>Support Request</p>
+          </label>
         </div>
-        <p class="error-text">Please select a query type</p>
+        <!-- <p class="error-text">Please select a query type</p> -->
       </div>
 
       <div class="input-container">
         <label for="message">message</label>
-        <textarea class="input-error" name="message" id="message"></textarea>
-        <p class="error-text">This field is required</p>
+        <textarea name="message" id="message" v-model="formData.message"></textarea>
+        <!-- <p class="error-text">This field is required</p> -->
       </div>
 
       <div class="input-container">
         <div class="flex items-center space-x-4">
-          <input type="checkbox" name="agreed" id="agreed" />
-          <label for="agreed">I consent to being contacted by the team</label>
+          <input type="checkbox" name="checked" id="checked" v-model="formData.checked" />
+          <label for="checked">I consent to being contacted by the team</label>
         </div>
-        <p class="error-text">To submit this form, please consent to being contacted</p>
+        <!-- <p class="error-text">To submit this form, please consent to being contacted</p> -->
       </div>
 
       <button class="btn" type="submit">Submit</button>
